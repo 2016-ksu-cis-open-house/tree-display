0a02c5dcf0fcc74ddc22b7afe490974da367cfa6
Kevin Dice <kevin.dice1@gmail.com>
2016-03-06 13:33:24 -0600

    Add socket.io connection

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('static'));
 
@@ -7,7 +9,10 @@ app.get('/', function (req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
-app.listen(3000, function(){
+http.listen(3000, function(){
   console.log('Listening on port 3000!');
 });
