0fd6249ce4390d77e1290336a9081ca863a8f827
Richard Petrie <deedasmi@gmail.com>
2016-03-07 16:15:52 -0600

    Loaded white_list and black_list into set, implemented black_list

--- app.js
+++ app.js
@@ -2,11 +2,33 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+var sets = require('simplesets');
 
 app.use(express.static('static'));
 app.set('view engine', 'jade');
 
 var names = [];
+
+// Initialize whitelist and black list
+var white_list = new sets.Set();
+load_file(white_list, 'special_names/whitelist.txt');
+var black_list = new sets.Set();
+load_file(black_list, 'special_names/blacklist.txt');
+
+function load_file(s, file) {
+  fs.readFile(file, 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    t = data.split("\n");
+    for (var i = 0; i < t.length; i++) {
+      s.add(t[i].toLowerCase());
+    }
+    console.log("Finished loading " + file);
+  });
+};
+
 
 app.get('/', function (req, res){
   res.render('index');
@@ -17,9 +39,17 @@ app.get('/tree', function (req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('A user connected from: ' + socket.handshake.address);
   socket.on('add name', function(msg){
+    console.log('Attempting to add name ' + msg);
+    if (black_list.has(msg.toLowerCase())) {
+       // TODO: Warn client
+       console.log("User entered a bad name");
+       return "Bad name";
+    }
+    // TODO: White list
     names.push(msg);
+    console.log("Good name");
     io.emit('add name', msg)
   });
 });
