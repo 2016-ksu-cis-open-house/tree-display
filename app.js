28f957c8012dfb2d769704399e06250e508d05fe
Kevin Dice <kevin.dice1@gmail.com>
2016-03-07 13:54:46 -0600

    Add name message passing and persistance

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ var io = require('socket.io')(http);
 app.use(express.static('static'));
 app.set('view engine', 'jade');
 
+var names = [];
+
 app.get('/', function (req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/tree', function (req, res){
-  res.sendFile(__dirname + '/tree.html');
+  res.render('tree', {names: names});
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('add name', function(msg){
+    names.push(msg);
     io.emit('add name', msg)
   });
 });
