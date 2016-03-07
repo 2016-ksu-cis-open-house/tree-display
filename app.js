bdb4729756d4ab9bb64d03fe64d193cd4a93b790
Kevin Dice <kevin.dice1@gmail.com>
2016-03-07 13:42:03 -0600

    Add jade dependency

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static('static'));
+app.set('view engine', 'jade');
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/tree', function (req, res){
+  res.sendFile(__dirname + '/tree.html');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('add name', function(msg){
+    io.emit('add name', msg)
+  });
 });
 
 http.listen(3000, function(){
