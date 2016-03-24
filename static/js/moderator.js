1f2256e6cee9e2cb88d7622c684d514e7bd407ae
Joy Hauser <joy.hauser@hotmail.com>
2016-03-23 22:24:51 -0500

    Update Moderator Form Button Functionality
    * Update accept button functionality to work with the current button
    * Update decline button functionality to work with the current button
    * Update comments for functionality

--- static/js/moderator.js
+++ static/js/moderator.js
@@ -1,16 +1,24 @@
 var socket = io();
 
-$('form').accept(function(e){
+$('#accept').click(function(e){
+  // Prevent JavaScript from doing normal functionality
   e.preventDefault();
+
+  // Emit a message to accept the current name
   socket.emit('accept name', 'Valid');
+
   // Disable form after first submission
   $('form').unbind('accept');
   $('form').accept(function(e){ e.preventDefault(); });
 });
 
-$('form').decline(function(e){
+$('#decline').click(function(e){
+  // Prevent JavaScript from doing normal functionality
   e.preventDefault();
+
+  // Emit a message to decline the current name
   socket.emit('decline name', 'Invalid');
+
   // Disable form after first submission
   $('form').unbind('decline');
   $('form').decline(function(e){ e.preventDefault(); });
