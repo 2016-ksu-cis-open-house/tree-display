bfd48c7e6861528e21474d6bb5077433c249d369
Joy Hauser <joy.hauser@hotmail.com>
2016-03-31 01:50:57 -0500

    sortNames: Remove the Print Statements
    * Remove the print statements that inform the user where
    they are in the code
    * Remove the requests for user input

--- special_names/sortNames.py
+++ special_names/sortNames.py
@@ -14,9 +14,6 @@ fileName = 'whitelist.txt'
 
 # Get names from raw_input file
 with open(fileName, 'r+') as input:
-    print "open file"
-    raw_input("Press enter...")
-
     # Get input from file
     for line in input:
         name = line.strip('\n')
@@ -25,26 +22,15 @@ with open(fileName, 'r+') as input:
         # in the list already
         if(name not in nameList):
             nameList.append(name)
-        print name
-    raw_input("Press enter...")
 
     # Empty file
     input.close()
     os.remove(fileName)
 
 with open(fileName, 'w') as output:
-    print "get input"
-    raw_input("Press enter...")
-
     # Sort the name list alphabetically
     nameList.sort()
 
-    print "sort input"
-    raw_input("Press enter...")
-
-    print "names:"
     # Put output in file
     for name in nameList:
         output.write(name + '\n')
-        print name
-    raw_input("Press enter...")
