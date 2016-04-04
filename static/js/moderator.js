6d41a0a4afd1c95c12aa454e6608bc7d9f371c57
Joy Hauser <joy.hauser@hotmail.com>
2016-04-04 00:05:46 -0500

    Moderator: Update comment for current name
    Update the comment for the variable that displays
    the current name to the moderator webpage.

--- static/js/moderator.js
+++ static/js/moderator.js
@@ -4,6 +4,7 @@ var nameInfo = {"name":"", "accepted":""};
 // Get the name from the server and put on the form for
 // the moderator to approve
 socket.on('moderate name', function(msg){
+  // Update the variable on the form for the current name
   $('#currentName').text(msg);
 
   // Put the current name in the data to be transmitted
