ab3651fdfd6285965dc3c78ede129951d35f4366
Kevin Dice <kevin.dice1@gmail.com>
2016-04-10 21:04:52 -0500

    Hooking up web socket handler

--- static/js/tree.js
+++ static/js/tree.js
@@ -1,6 +1,13 @@
 var socket = io();
 
+
+var $addField = $($('input')[0]);
+var $addButton = $($('input')[1]);
+var $removeField = $($('input')[2]);
+var $removeButton = $($('input')[3]);
+
+
 // Add a name to the tree
 socket.on('add name', function(msg){
-  $('#names').append($('<li>').text(msg));
+  console.log(msg);
 });
